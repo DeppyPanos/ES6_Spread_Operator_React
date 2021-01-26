00d85953df1ad41fa1f59efed50456ff8fa7e6a3
Deppy <50961367+DeppyPanos@users.noreply.github.com>
2021-01-26 17:16:05 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//Spread Operator
+//arrays
+// const citrus = ["lemon", "lime"];
+// const fruits = ["apple", "melon", ...citrus];
+
+// console.log(fruits);
+
+//objects
+// const fullName = {
+//   fName: "Deppy",
+//   lName: "Tsou"
+// };
+
+// const user = {
+//   ...fullName,
+//   username: "deppytsou"
+// };
+
+// console.log(user);
